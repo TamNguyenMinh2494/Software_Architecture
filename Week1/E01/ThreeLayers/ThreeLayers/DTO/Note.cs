5f5fd3fc0ffb563e778886ea3743223310a43b7c
Nguyễn Minh Tâm <tam.nm@itsstraining.edu.vn>
2019-09-28 14:35:08 +0700

    week2

--- Week1/E01/ThreeLayers/ThreeLayers/DTO/Note.cs
+++ Week1/E01/ThreeLayers/ThreeLayers/DTO/Note.cs
@@ -3,16 +3,3 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace ThreeLayers.DTO
-{
-    class Note
-    {
-        public int ID { get; set; }
-        public String Title { get; set; }
-        public String Creator { get; set; }
-        public String Content { get; set; }
-        public String Date { get; set; }
-        public bool IsSharable { get; set; }
-    }
-}
