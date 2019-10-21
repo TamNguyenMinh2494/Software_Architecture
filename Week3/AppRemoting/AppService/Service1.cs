ac779c8ead3ad987858db5f73fb5c2a4c3d1b707
Nguyễn Minh Tâm <tam.nm@itsstraining.edu.vn>
2019-10-21 11:06:11 +0700

    Using AppService instead of AppServer

---
+++ Week3/AppRemoting/AppService/Service1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+namespace AppService
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            ChannelServices.RegisterChannel(new TcpChannel(1234), false);
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(NoteBUS), "xxx", WellKnownObjectMode.Singleton);
+            RemotingConfiguration.CustomErrorsMode = CustomErrorsModes.Off;
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
