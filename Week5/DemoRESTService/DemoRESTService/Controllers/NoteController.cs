e396136e1323d585ac7ea7c6251feda320d8e9da
Nguyễn Minh Tâm <tam.nm@itsstraining.edu.vn>
2019-11-15 21:26:19 +0700

    CRUD with REST api

---
+++ Week5/DemoRESTService/DemoRESTService/Controllers/NoteController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using DemoRESTService.Models;
+using DemoRESTService.DataAccessLayer;
+
+namespace DemoRESTService.Controllers
+{
+    public class NoteController : ApiController
+    {
+        [Route("api/notes")]
+        [HttpGet]
+        public List<Note> GetAll()
+        {
+            List<Note> notes = new NoteDAO().GetAllNotes();
+            return notes;
+
+        }
+
+        [Route("api/notes/{id}")]
+        [HttpGet]
+        public Note NoteDetails(int id)
+        {
+            Note note = new NoteDAO().DetailNote(id);
+            return note;
+
+        }
+
+        [Route("api/notes")]
+        [HttpPost]
+        public bool AddNewNote(Note newNote)
+        {
+            bool note = new NoteDAO().AddNote(newNote);
+            return note;
+
+        }
+
+        [Route("api/notes/{id}")]
+        [HttpDelete]
+        public bool DeleteNote(int id)
+        {
+            bool result = new NoteDAO().DeleteNote(id);
+            return result;
+        }
+
+        [Route("api/notes/{id}")]
+        [HttpPut]
+        public bool UpdateNote(int id, Note newNote)
+        {
+            bool result = new NoteDAO().Update(id, newNote);
+            return result;
+        }
+
+        [Route("api/notes")]
+        [HttpPut]
+        public bool UpdateNote(Note newNote)
+        {
+            bool result = new NoteDAO().Update(newNote);
+            return result;
+        }
+
+        [Route("api/notes/search/{keyword}")]
+        [HttpGet]
+        public List<Note> Search(string keyword)
+        {
+            List<Note> notes = new NoteDAO().SearchNote(keyword);
+            return notes;
+        }
+    }
+}
---
+++ Week5/DemoRESTService/DemoRESTService/DataAccessLayer/NoteDAO.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using DemoRESTService.Models;
+
+namespace DemoRESTService.DataAccessLayer
+{
+
+    public class NoteDAO
+    {
+        private NoteDBDataContext db = new NoteDBDataContext(ConfigurationManager.ConnectionStrings["strCon"].ConnectionString);
+        public List<Note> GetAllNotes()
+        {
+            List<Note> notes = db.Notes.ToList();
+            return notes;
+        }
+        public Note DetailNote(int id)
+        {
+            Note note = db.Notes.SingleOrDefault(x => x.ID == id);
+            return note;
+        }
+
+        public bool AddNote(Note newNote)
+        {
+            try {
+                db.Notes.InsertOnSubmit(newNote);
+                db.SubmitChanges();
+                return true;
+            } catch {
+                return false;
+            }
+        }
+
+        public bool DeleteNote(int id)
+        {
+            Note note = db.Notes.SingleOrDefault(x => x.ID == id);
+            if (note == null) return false;
+            try
+            {
+                db.Notes.DeleteOnSubmit(note);
+                db.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Update(int id, Note newNote)
+        {
+            if (id != newNote.ID) return false;
+            Note note = db.Notes.SingleOrDefault(x => x.ID == id);
+            if (note == null) return false;
+            note.Title = newNote.Title;
+            note.Creator = newNote.Creator;
+            note.Content = newNote.Content;
+            note.Date = newNote.Date;
+            note.IsSharable = newNote.IsSharable;
+            try
+            {
+                db.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        // Update without id on routing api
+        public bool Update(Note newNote)
+        {
+            
+            Note note = db.Notes.SingleOrDefault(x => x.ID == newNote.ID);
+            if (note == null) return false;
+            note.Title = newNote.Title;
+            note.Creator = newNote.Creator;
+            note.Content = newNote.Content;
+            note.Date = newNote.Date;
+            note.IsSharable = newNote.IsSharable;
+            try
+            {
+                db.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public List<Note> SearchNote(string keyword)
+        {
+            List<Note> notes = db.Notes.Where(i => i.Title.Contains(keyword)).ToList();
+            return notes;
+        }
+    }
+} 
